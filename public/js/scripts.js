f101c55b576ecff820e7ee48c1975f57f8912445
Rodrigo M. Rodrigues <rmirandarodrigues@outlook.com>
2021-07-21 10:51:55 -0300

    Implement success/fail pop up

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,28 +1,25 @@
 
-// document.addEventListener("DOMContentLoaded", function(event){
-//     // console.log("RUNNING CONTENTLOAD");
-//     const alertListagem = document.querySelector('#alertListagem');
-//     alertListagem.style.display = 'none';
-    
-//     const alertCadastro = document.querySelector('#alertCadastro');
-//     alertCadastro.style.display = 'none';
+document.addEventListener("DOMContentLoaded", function(event){
+    if(location.href.indexOf('delete=true') != -1){
+        displayMessage("alertListagem", "Country deleted sucessfully");
+    }
+    else if(location.href.indexOf('edit=true') != -1){
+        displayMessage("alertListagem", "Country edited sucessfully");
+    }
+    else if(location.href.indexOf('new=true') != -1){
+        displayMessage("alertListagem", "Country added sucessfully");
+    }
+    else if(location.href.indexOf('erro') != -1){    
+        displayMessage("alertCadastro", "There was an error!");
+    }
+}); // End of DOMContentLoaded
 
-//     const frmCadastro = document.getElementById("frmCadastro");
-    
-//     frmCadastro.onsubmit = (evt) => {
-//         // evt.preventDefault();
-//     }
-// }); // End of DOMContentLoaded
-// alert("TEST");
-if(location.href.indexOf('delete=true') != -1){
-    alert('Cliente excluído com sucesso!');
-}
-else if(location.href.indexOf('edit=true') != -1){
-    alert('Cliente editado com sucesso!');
-}
-else if(location.href.indexOf('new=true') != -1){
-    alert('Cliente cadastrado com sucesso!');
-}
-else if(location.href.indexOf('erro') != -1){
-    alert('Ocorreu um erro!');
+
+function displayMessage(selector, msg) {
+    let display = document.getElementById(selector);
+    display.innerText = msg;
+    display.style.display = 'block';
+    setTimeout(() => {
+        display.style.display = 'none';
+    }, 2000);
 }
